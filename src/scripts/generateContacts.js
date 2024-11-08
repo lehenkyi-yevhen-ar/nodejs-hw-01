6e16ce7f917cf7031ece83521de06986dbfa9b61
lehenkyi-yevhen-ar <meister.ramires@gmail.com>
2024-11-08 20:14:31 +0200

    add utils and scripts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,23 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    const contacts = await readContacts();
+
+    const newContacts = [];
+
+    for (let i = 0; i < number; i++) {
+      newContacts.push(createFakeContact());
+    }
+
+    const updatedContacts = [...contacts, ...newContacts];
+
+    await writeContacts(updatedContacts);
+  } catch (err) {
+    console.error('error:', err);
+  }
+};
 
 generateContacts(5);
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,14 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+    const updatedContacts = contacts.slice(0, -1);
+    await writeContacts(updatedContacts);
+  } catch (err) {
+    console.error('error:', err);
+  }
+};
 
 removeLastContact();
